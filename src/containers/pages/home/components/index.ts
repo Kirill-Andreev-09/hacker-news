99328df91b5cb02ac6d11111dbfc607e65994573
yoyohoho84 <kirill_1131@mail.ru>
2023-04-30 17:20:02 +0300

    Updates

--- src/containers/pages/home/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './news-list';
-export * from './news-list-item';
-export * from './update-button';
--- src/contexts/index.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { UserStore } from '../stores';
-
-export const stores = {
-  UserStore: new UserStore()
-};
-
-export const storesContext = React.createContext(stores);
